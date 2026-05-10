ce3f74337a679a29299104a3c4e0a3984e296741
rchmeis <r.chmeis@gmail.com>
2026-05-10 13:01:43 +0200

    Corrected Venue - cleaner code.

--- CircusLunaConsole/Program.cs
+++ CircusLunaConsole/Program.cs
@@ -7,9 +7,9 @@ Console.WriteLine(A1.ToString());
 Employee E1 = new Employee("IBAN3264234", "amdin", "Lars Petersen", "55555555", "Lars@cirkusluna.dk");
 Console.WriteLine(E1.ToString());
 
-//Venue TeltA = new Venue("TeltA");
+//Venue TeltA = new Venue("TeltA", 150, 10);
 //List<Seat> allSeats = TeltA.AllSeats;
-//foreach(Seat s in allSeats) { Console.WriteLine(s.ToString()); }
+//foreach (Seat s in allSeats) { Console.WriteLine(s.ToString()); }
 
 DateTime post1Date = DateTime.Now;
 BlogPost post1 = new BlogPost("Cirkus Luna kommer til Ringsted!", "Cirkus Luna er vokset og kan nu besøge flere byer, herunder Ringsted! Skynd jer at booke billetter og glæd jer til fantastisk børnevenlig underholdning for hele familien", post1Date);
--- CircusLunaLibrary/Models/Venue.cs
+++ CircusLunaLibrary/Models/Venue.cs
@@ -11,30 +11,29 @@ namespace CircusLunaLibrary.Models
         public List<Seat> AllSeats { get; private set; } = new List<Seat>();
         public string Name { get; set; }
 
-        public Venue(string name)
+        public Venue(string name, int standardSeatCount, int vipSeatCount)
         {
             Name = name;
-            StandardSeatCount = 150;
-            VipSeatCount = 10;
+            StandardSeatCount = standardSeatCount;
+            VipSeatCount = vipSeatCount;
             InitializeSeats();
         }
         public void InitializeSeats()
         {
-            for(int i=1; i <= VipSeatCount; i++)
+            AddSeatsToSection("VIP", VipSeatCount, SeatType.VIP);
+
+            int seatCountPerSection = StandardSeatCount / 3;
+            AddSeatsToSection("A", seatCountPerSection, SeatType.standard);
+            AddSeatsToSection("B", seatCountPerSection, SeatType.standard);
+            AddSeatsToSection("C", seatCountPerSection, SeatType.standard);         
+           
+        }
+
+        public void AddSeatsToSection(string sectionName, int count, SeatType type)
+        {
+            for(int i = 1; i <= count; i++)
             {
-                AllSeats.Add(new Seat("VIP", i.ToString(), SeatType.VIP));
-            }
-            for(int i=1; i<=50; i++)
-            {
-                AllSeats.Add(new Seat("A", i.ToString(), SeatType.standard));
-            }
-            for(int i=51; i<=100; i++)
-            {
-                AllSeats.Add(new Seat("B", i.ToString(), SeatType.standard));
-            }
-            for (int i = 101; i <= 150; i++)
-            {
-                AllSeats.Add(new Seat("C", i.ToString(), SeatType.standard));
+                AllSeats.Add(new Seat(sectionName, i.ToString(), type));
             }
         }
         public override string ToString()
