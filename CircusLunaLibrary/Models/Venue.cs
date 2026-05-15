08841be7d2d9b9975ab631ee4679bff2f8e0d85e
rchmeis <r.chmeis@gmail.com>
2026-05-15 16:33:29 +0200

    Changed display in BookSeats. Corrected commentedOut code in Venue (flexible calculation of seats/rows)

--- CircusLunaLibrary/Models/Venue.cs
+++ CircusLunaLibrary/Models/Venue.cs
@@ -73,23 +73,24 @@ namespace CircusLunaLibrary.Models
 			}
 		}
         //public void InitializeSeats()
-		//kræver VipSeats og StandardSeats properties. Mulighed for flere telte.
+        //kræver VipSeats og StandardSeats properties. Mulighed for flere telte.
         //{
         //    int seatsPerRow = 10;
+        //
         //    for (int i = 0; i < VipSeats; i++)
         //    {
-        //        AllSeats.Add(new Seat('0', i, SeatType.VIP));
+        //        AllSeats.Add(new Seat('V', i+1, SeatType.VIP));
         //    }
 
         //    for (int i = 0; i < StandardSeats; i++)
         //    {
-        //        char charRow = (char)('A' + (i / seatsPerRow)); //TYPE CASTING: computeren ser chars som tal. Derfor A+1=B. (char) er typecasting. Vi caster tallet til en char efter udregningen.
-        //        int seatNumber = (i % seatsPerRow) + 1;  //MODULUS: Vi tager det, der er tilbage. 0/10=0+1 -> nr 1. 5/10=5+1 -> nr 6. 27/10=7+1 -> nr 8 osv. Ignorer 10'erne som udgør ROWS.
-        //        if (charRow == 'A' && (i >= 0 && i <= 9))
-        //            AllSeats.Add(new Seat(charRow, seatNumber, SeatType.standard));
-
-        //    }
+    //			char charRow = (char)('A' + (i / seatsPerRow)); 
+    //			int seatNumber = (i % seatsPerRow) + 1;  
+    //			AllSeats.Add(new Seat(charRow, seatNumber, SeatType.standard));
+        //     }
+        //		
         //}
 
-    }
+    }//TYPE CASTING: computeren ser chars som tal. Derfor A+1=B. (char) er typecasting. Vi caster tallet til en char efter udregningen.
+     //MODULUS: Vi tager det, der er tilbage. 0%10=0+1 -> nr 1. 5%10=5+1 -> nr 6. 27%10=7+1 -> nr 8 osv. 10 er en hel "pakke". hver gang en hel "pakke" udfyldes, ignorerer vi det, og kigger på hvad der er tilbage.
 }
