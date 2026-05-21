823a29ac9e5e81cdfa716e28acedc869fb140dbb
rchmeis <r.chmeis@gmail.com>
2026-05-21 21:55:12 +0200

    Homemade Bubblesort algorithm added to personService. (sortByName).

--- CircusLunaLibrary/Services/PersonService.cs
+++ CircusLunaLibrary/Services/PersonService.cs
@@ -55,5 +55,48 @@ namespace CircusLunaLibrary.Services
         {
             _personRepository.UpdatePerson(id, employee);
         }
+        public List<Person> SortByNameAZ()
+        {
+            List<Person> allPeople = new List<Person>();
+            int counter = 0;
+            while (counter < allPeople.Count)
+            {
+                int iterations = allPeople.Count - counter;
+                for (int i = 0; i < iterations - 1; i++)
+                {
+                    char[] person0 = allPeople[i].Name.ToLower().ToCharArray();
+                    char[] person1 = allPeople[i + 1].Name.ToLower().ToCharArray();
+
+                    int shortestArray = 0;
+                    if (person0.Length < person1.Length)
+                    {
+                        shortestArray = person0.Length;
+                    }
+                    else
+                    {
+                        shortestArray = person1.Length;
+                    }
+                    //int shortestArray = Math.Min(person0.Length, person1.Length);
+
+                    for (int j = 0; j < shortestArray; j++)
+                    {
+                        if ((person0[j] < person1[j]))
+                        {
+                            break; // Already in the right order, break the loop
+                        }
+                        else if (person1[j] < person0[j])
+                        {
+                            Person person = allPeople[i];
+                            allPeople[i] = allPeople[i + 1];
+                            allPeople[i + 1] = person;
+                            break;
+                        }
+
+                    }
+                }
+                counter++;
+            }
+            return allPeople;
+        }
     }
 }
